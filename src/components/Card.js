3587174aa4fe20b1c368a42ff4b29933a2bb1b74
prayge <sgjfxvs@gmail.com>
2022-07-03 05:27:52 +0100

    spacings

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,16 +4,18 @@ import "./Card.scss";
 
 function Card(props) {
 	return (
-		<div className="card">
-			<div className="card__body">
-				<img src={props.img} className="card__image" alt="img" />
-				<h2 className="card__title">
-					{props.title}
-					<p className="card__description">{props.description}</p>
-				</h2>
-			</div>
-			<div className="card__center">
-				<button className="card__btn">Explore</button>
+		<div className="card-container">
+			<div className="card">
+				<div className="card__body">
+					<img src={props.img} className="card__image" alt="img" />
+					<h2 className="card__title">
+						{props.title}
+						<p className="card__description">{props.description}</p>
+					</h2>
+				</div>
+				<div className="card__center">
+					<button className="card__btn">Explore</button>
+				</div>
 			</div>
 		</div>
 	);
--- src/components/SkillsBar.js
+++ src/components/SkillsBar.js
@@ -3,53 +3,63 @@ import "./SkillsBar.css";
 
 function SkillsBar() {
 	return (
-		<div className="skills-bar">
-			<div className="bar">
-				<div className="info">
-					<span>Python</span>
+		<div className="skills-container">
+			<div className="skills-bar">
+				<div className="bar">
+					<div className="info">
+						<span>Python</span>
+					</div>
+					<div className="progress-line python">
+						<span></span>
+					</div>
 				</div>
-				<div className="progress-line python">
-					<span></span>
+				<div className="bar">
+					<div className="info">
+						<span>PyTorch</span>
+					</div>
+					<div className="progress-line pt">
+						<span></span>
+					</div>
 				</div>
-			</div>
-			<div className="bar">
-				<div className="info">
-					<span>C++</span>
+				<div className="bar">
+					<div className="info">
+						<span>C++</span>
+					</div>
+					<div className="progress-line cpp">
+						<span></span>
+					</div>
 				</div>
-				<div className="progress-line cpp">
-					<span></span>
+				<div className="bar">
+					<div className="info">
+						<span>HTML</span>
+					</div>
+					<div className="progress-line html">
+						<span></span>
+					</div>
 				</div>
-			</div>
-			<div className="bar">
-				<div className="info">
-					<span>HTML</span>
+				<div className="bar">
+					<div className="info">
+						<span>CSS</span>
+					</div>
+					<div className="progress-line css">
+						<span></span>
+					</div>
 				</div>
-				<div className="progress-line html">
-					<span></span>
+				<div className="bar">
+					<div className="info">
+						<span>React.js</span>
+					</div>
+					<div className="progress-line react">
+						<span></span>
+					</div>
 				</div>
-			</div>
-			<div className="bar">
-				<div className="info">
-					<span>CSS</span>
-				</div>
-				<div className="progress-line css">
-					<span></span>
-				</div>
-			</div>
-			<div className="bar">
-				<div className="info">
-					<span>React.js</span>
-				</div>
-				<div className="progress-line react">
-					<span></span>
-				</div>
-			</div>
-			<div className="bar">
-				<div className="info">
-					<span>JavaScript</span>
-				</div>
-				<div className="progress-line js">
-					<span></span>
+				<div className="bar">
+					<div className="info">
+						<span>JavaScript</span>
+					</div>
+					<div className="progress-line js">
+						<span></span>
+					</div>
 				</div>
 			</div>
 		</div>
