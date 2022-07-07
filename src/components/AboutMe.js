ea2e6ddcbaf7091fb943dabee2454f12e74cf84d
prayge <sgjfxvs@gmail.com>
2022-07-07 09:32:10 +0100

    About done

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,24 +5,37 @@ import "./AboutMe.css";
 
 function AboutMe() {
 	return (
-		<div className="about-me-container">
+		<div>
 			<Margin />
 			<SectionTitle title="ABOUT" extra="me" />
-			<div className="about-me">
-				<h1>Hi, im Samuel Farquhar</h1>
-				<p>
-					Im from Edinburgh in Scotland, one of the most beautiful cities in the
-					world. I love problem solving and incorporating my ideas into a
-					practical solution.
-				</p>
-				<p>
-					I've had a passion for healthcare and medicine since i was young and
-					would like to help others out there using my knowledge of programming
-				</p>
-				<p>
-					I try to expand my knowledge constantly working hard at many different
-					things to expand my ability to learn and adapt to new challenges.
-				</p>
+			<div className="container">
+				<div className="about-me-flex">
+					<div className="about-me">
+						<h1>Hi, im Samuel Farquhar</h1>
+						<p>
+							Im from Edinburgh in Scotland, one of the most beautiful cities in
+							the world. I love problem solving and incorporating my ideas into
+							a practical solution.
+						</p>
+						<p>
+							I've had a passion for healthcare and medicine since i was young
+							and would like to help others out there using my knowledge of
+							programming
+						</p>
+						<p>
+							I try to expand my knowledge constantly working hard at many
+							different things to expand my ability to learn and adapt to new
+							challenges.
+						</p>
+					</div>
+					<div className="about-me-side">
+						<img
+							className="about-me-side image"
+							src="../../images/edi.png"
+							alt=""
+						/>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
