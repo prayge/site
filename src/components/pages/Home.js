5833939376e347a9a365fb3b84b334b3ee58d25d
prayge <sgjfxvs@gmail.com>
2022-07-07 07:27:57 +0100

    CSS

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,14 +3,16 @@ import React from "react";
 import HeroSection from "../HeroSection";
 import SkillsBar from "../subcomponent/SkillsBar";
 import CardSection from "../subcomponent/CardSection";
+import Margin from "../style-components/Margin";
 
 function Home() {
 	return (
 		<>
 			<HeroSection />
+			<Margin />
 			<SkillsBar />
+			<div className="spacer" style={{ marginTop: 250 }} />
 			<CardSection />
-			
 		</>
 	);
 }
