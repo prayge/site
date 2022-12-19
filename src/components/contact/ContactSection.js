7a5320880a77bb4fbaf079ae76a95e84805f37b7
prayge <sgjfxvs@gmail.com>
2022-12-19 20:18:03 +0000

    rmv contact

--- src/components/contact/ContactSection.js
+++ src/components/contact/ContactSection.js
@@ -1,9 +1,9 @@
 import React from "react";
 import SectionTitle from "../SectionTitle";
 import ContactInfo from "./ContactInfo";
-import { MdPhoneIphone } from "react-icons/md";
+//import { MdPhoneIphone } from "react-icons/md";
 import { MdLocationPin } from "react-icons/md";
-import { MdAlternateEmail } from "react-icons/md";
+//import { MdAlternateEmail } from "react-icons/md";
 import ContactForm from "./ContactForm";
 import "./ContactSection.css";
 
@@ -13,11 +13,6 @@ function ContactSection() {
 			<SectionTitle title="CONTACT" subtitle="get in " />
 			<div className="contact-wrapper">
 				<div className="left">
-					<ContactInfo
-						text="samuel_farquhar@aol.com"
-						icon={<MdAlternateEmail size={30} />}
-					/>
-					<ContactInfo text="07943800980" icon={<MdPhoneIphone size={30} />} />
 					<ContactInfo
 						text="Edinburgh, Scotland"
 						icon={<MdLocationPin size={30} />}
@@ -32,3 +27,9 @@ function ContactSection() {
 }
 
 export default ContactSection;
+
+// <ContactInfo
+// 						text="samuel_farquhar@aol.com"
+// 						icon={<MdAlternateEmail size={30} />}
+// 					/>
+// 					<ContactInfo text="07943800980" icon={<MdPhoneIphone size={30} />} />
